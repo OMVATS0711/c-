a6cf4e911e70f42edb2dfe5cf47ce0d784c1ed61
Om Vats <144024782+OMVATS0711@users.noreply.github.com>
2025-04-01 12:32:59 +0530

    Add files via upload

---
+++ missing_number_array.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=6;
+    int arr[]={1,2,3,4,5};
+    //sum of array
+    int sum=0;
+    for(int i=0;i<n-1;i++){
+        sum+=arr[i];
+    }
+    //sum of n number
+    int ans=n*(n+1)/2;
+    cout<< ans-sum;
+    return 0;
+    
+}
